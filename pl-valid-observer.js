212817e1457e39b4c738a6aec05e995deb200b74
Dmitry Boikov <boikovdv@gmail.com>
2026-03-25 16:30:14 +0300

    support polylib 1.2.0 without dom-change event in repeater

--- pl-valid-observer.js
+++ pl-valid-observer.js
@@ -1,39 +1,43 @@
-import { PlElement, css } from "polylib";
+import { PlElement, css } from 'polylib';
 
 class PlValidObserver extends PlElement {
-    static get properties() {
-        return {
-            invalid: {
-                type: Boolean,
-                value: false
-            },
-            targetContainer: {
-                type: Object
-            }
-        }
-    }
+    static properties = {
+        invalid: { type: Boolean, value: false },
+        targetContainer: { type: Object }
+    };
 
-    static get css(){
-        return css`
-            :host {
-                display: none;
-            }
-        `;
-    }
+    static css = css`
+        :host {
+            display: none;
+        }
+    `;
 
     connectedCallback() {
         super.connectedCallback();
         this.target = this.targetContainer || this.parentNode;
         this.target.addEventListener('validation-changed', this._handlerValid.bind(this));
-        this.target.addEventListener('dom-changed', this._handlerValid.bind(this));
+        if (!this.registerHook) {
+            // Polylib before v1.2.0 doesn't have hook support, use repeater dom-changed event
+            this.target.addEventListener('dom-changed', this._handlerValid.bind(this));
+        }
     }
 
-    _handlerValid() {
-        if (this._awaiter)
-            clearTimeout(this._awaiter);
+    #watchList = new Set();
+    _handlerValid(event) {
+        if (this._awaiter) clearTimeout(this._awaiter);
+
+        // Подписываемся на хук компонента, поскольку событие из него не дойдет при удалении из DOM дерева
+        if (event && !this.#watchList.has(event.target)) {
+            event.target.registerHook?.('disconnected', () => {
+                this._handlerValid();
+                this.#watchList.delete(event.target);
+            });
+            this.#watchList.add(event.target);
+        }
+
         this._awaiter = setTimeout(() => {
-            let cmps = [...this.target.querySelectorAll('*')];
-            this.invalid = cmps.some(x => x != this && x.invalid);
+            const cmps = [...this.target.querySelectorAll('*')];
+            this.invalid = cmps.some(x => x !== this && x.invalid);
         }, 100);
     }
 }
